8cdd34a2e436f9fdf31ade81bab5273f401e455c
robmi11 <robert.mizianty@gmail.com>
2024-03-09 22:21:35 +0100

    Express server with some basic routes.

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is on");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json([products]);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+
+  if (id < 1 || id > 7 || isNaN(id)) {
+    res.status(401).json({ message: "Product not found" });
+  } else {
+    const product = products.find((item) => item._id === id);
+    res.status(200).json(product);
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is on and listens at port: ${PORT}`),
+);
